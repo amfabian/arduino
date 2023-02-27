396f65d71663ab90e56c0c8f16c77612ec9e56e5
Alexandre Fabian <xandy.fabian@gmail.com>
2023-02-27 17:48:06 -0300

    Add files via upload

---
+++ test_temp_sensor.ino
@@ -0,0 +1,39 @@
+#include "DHT.h"
+#define DHTPIN 2 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  dht.begin();
+}
+ void loop() 
+{
+  int sensorValue;
+  sensorValue=analogRead(A0);
+  // A leitura da temperatura e umidade pode levar 250ms!
+  // O atraso do sensor pode chegar a 2 segundos.
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // testa se retorno é valido, caso contrário algo está errado.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Failed to read from DHT");
+  } 
+  else
+  {
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.print("%t ");
+    Serial.print(" -- Temperatura: ");
+    Serial.print(t);
+    Serial.print("*C");
+    Serial.print(" -- UV:");
+    Serial.print(sensorValue*5000/1023.0);
+    Serial.print("mV");
+    Serial.print("\n");
+    delay(1000);
+  }
+}
---
+++ test_uv_sensor.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int sensorValue;
+
+sensorValue=analogRead(A0);
+Serial.print("The voltage value:");
+Serial.print(sensorValue*5000/1023.0);
+Serial.println("mV");
+delay(1000);
+Serial.print("\n");
+}
